7f261d180df112e6936a2037d969a950ac73ae13
LeQP <qle@uoregon.edu>
2022-01-14 12:56:36 -0800

    another test

--- ExtractFileTest.py
+++ ExtractFileTest.py
@@ -3,8 +3,8 @@ from cgi import test
 
 def printTests():
     print("this is a test")
-    print("this is a test 2")
     print("another test")
+    # added a comment
 
 print("hello world")
 printTests()
